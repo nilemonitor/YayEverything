9749b8a64ed12f263af0899b76fe339777156308
Christine Lindgren <christinemlindgren@gmail.com>
2013-03-19 20:24:04 -0400

    added home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 YayEverything::Application.routes.draw do
+ get 'about' => 'pages#about'
+
+
+  root :to => 'Pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
